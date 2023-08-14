a7fd42276bab722df43f350858f2d180a103dc1b
MOHAMMADIzahra1401 <140957622+MOHAMMADIzahra1401@users.noreply.github.com>
2023-08-14 06:18:15 -0700

    Add files via upload

---
+++ tadadkalamat.py
@@ -0,0 +1,5 @@
+print("Enter a jomle  and shoroe an baiad bedon fasele bashad")
+jomle=input("Entre  words  :")
+tedad_kalamat=jomle.split(" ")
+print(tedad_kalamat)
+print("tedad_kalamat is :",len(tedad_kalamat))
